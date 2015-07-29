8dbea1077a9c8e382070482ad4817f736be8e392
njanetos <nejanetos@gmail.com>
2015-07-29 12:31:15 -0400

    Added stable roommate code

---
+++ src/stable.h
@@ -0,0 +1,8 @@
+#ifndef stable_h
+#define stable_h
+
+List stableRoommateMatching(const umat pref);
+void deleteValueWithWarning(std::vector<size_t> *vec, size_t val);
+bool isEmpty(std::vector< std::vector<size_t> > *table);
+
+#endif
