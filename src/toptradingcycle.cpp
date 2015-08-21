d5d2c76cfb6dc77212e3539c57fd2c8555153654
njanetos <nejanetos@gmail.com>
2015-08-21 15:38:39 -0400

    Expanded psuedocode.

--- src/toptradingcycle.cpp
+++ src/toptradingcycle.cpp
@@ -28,31 +28,50 @@ List topTradingCycle(const umat pref) {
     matchings.fill(-1);
     
     // used for the algorithm below
-    uword current_agent;
+    uword current_agent = -1;
     
     // loop until everyone's been matched
     while (true) {
         
-        // identify rotations, maximum length of N
+        // if current_agent = -1, then set current_agent to be
+        // the first unmmatched guy
+        
+        // now identify rotations
         while(true) {
-            // start cycling through preferences, starting with the first unmatched guy
+            // start cycling through preferences, starting with current_agent
             
-            // find the agent's most preferred, unmatched outcome
+            // find current_agent's most preferred, unmatched outcome, p
             
-            // check if that guy has already shown up in this chain by checking if
-            // matchings is larger than -1
+            // set matchings[current_agent] = p
+            
+            // check if p has already shown up in this chain by checking if
+            // matchings[p] is larger than -1. if it is larger than -1, then
+            // that agent, who we know is unmmatched, must already have shown up
+            // somewhere in this loop. if matchings[p] is equal to -1, then that agent
+            // has never shown up in a loop and we can continue
             
             // if matchings is larger than -1, then we have a rotation, starting
-            // with the value of matchings, and ending with this agent
+            // with p, and ending with current_agent, so break
+            
+            // otherwise, continue looking for a rotation
         }
         
-        // loop through, and set is_matched to 1 for everyone in
-        // the rotation
+        // loop through, starting with p, then matchings[p], etc., and
+        // ending with current_agent. for each agent, set is_matched to
+        // 1. 
         
-        // check if everyone's matched
+        // check if everyone's matched, if so, we're done, so break
         if (sum(unmatched) == N) break;
         
-        // otherwise, set current_agent back and start again
+        // otherwise, we need to set current_agent in such a way so as to continue
+        // looking for rotations
+        
+        // one way to do this would be to check if (1-is_matched) .* matchings = -1*sum(1-is_matched)
+        // if true, then set current_agent equal to the first unmmatched agent
+        // otherwise, we just cut off the 'tail' when we removed the rotation, and the body
+        // can be used to find a new rotation
+        // in this case, set current_agent to be the last agent in the head, i.e., the agent who we 
+        // matched to p. 
     }
     
     return 0;
