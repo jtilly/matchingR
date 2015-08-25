be7bd3704205bc1db9aa91c8cb75a4a6e767ae6e
njanetos <nejanetos@gmail.com>
2015-08-25 16:36:13 -0400

    Switched find back to for loop once I discovered how it actually works.

--- src/toptradingcycle.cpp
+++ src/toptradingcycle.cpp
@@ -58,8 +58,13 @@ List topTradingCycle(const umat pref) {
             
             // find current_agent's most preferred, unmatched outcome, p
             // provisionally match current_agent to p by setting matchings[current_agent] = p
-            matchings(current_agent) = as_scalar(find(is_matched(pref.col(current_agent)) == 0, 1));
-            
+            for (uword i = 0; i < N; ++i) {
+            if (is_matched(pref(i, current_agent)) == 0) {
+                    matchings(current_agent) = pref(i, current_agent);
+                    break;
+                }
+            }
+
             // logg.info() << "Current agent is " << current_agent << ".";
             
             // logg.info() << "Agent " << current_agent << " most prefers " << matchings(current_agent) << ".";
