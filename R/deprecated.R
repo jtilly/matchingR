5bd9a78d02882b437e5c746dd20dcf6124d17f9a
Jan Tilly <jantilly@gmail.com>
2015-09-28 09:52:34 -0400

    Bundled all deprecated functions into one file
    - Wrote a catch-all documentation for all of them
    - This has the advantage that the deprecated functions don't show up in the table of contents of the PDF documentation anymore

---
+++ R/deprecated.R
@@ -0,0 +1,88 @@
+#' Deprecated Functions in matchingR
+#'
+#' These functions are provided for compatibility with older version of
+#' the matchingR package. Eventually, these functions will be removed.
+#'
+#' @rdname matchingR-deprecated
+#' @name matchingR-deprecated
+#' @param ... generic set of parameters --- see documentation of new functions
+#' @docType package
+#' @aliases validateInputs checkStability checkPreferenceOrder one2many many2one
+#'   one2one galeShapleyMatching stableRoommateMatching onesided
+#'   checkStabilityRoommate validateInputsOneSided checkPreferenceOrderOnesided
+#'   topTradingCycle checkStabilityTopTradingCycle
+#' @section Details:
+#' \tabular{rl}{
+#'   \code{validateInputs} \tab was replaced by \code{\link{galeShapley.validate}}\cr
+#'   \code{checkStability} \tab was replaced by \code{\link{galeShapley.checkStability}}\cr
+#'   \code{checkPreferenceOrder} \tab was replaced by \code{\link{galeShapley.checkPreferences}}\cr
+#'   \code{one2many} \tab now mapped into \code{\link{galeShapley.collegeAdmissions}}\cr
+#'   \code{many2one} \tab now mapped into \code{\link{galeShapley.collegeAdmissions}}\cr
+#'   \code{one2one} \tab was replaced by \code{\link{galeShapley.marriageMarket}}\cr
+#'   \code{galeShapleyMatching} \tab was replaced by \code{\link{cpp_wrapper_galeshapley}}\cr
+#'   \code{stableRoommateMatching} \tab was replaced by \code{\link{cpp_wrapper_irving}}\cr
+#'   \code{onesided} \tab was replaced by \code{\link{roommate.matching}}\cr
+#'   \code{checkStabilityRoommate} \tab was replaced by \code{\link{cpp_wrapper_irving_check_stability}}\cr
+#'   \code{validateInputsOneSided} \tab was replaced by \code{\link{roommate.validate}}\cr
+#'   \code{checkPreferenceOrderOnesided} \tab was replaced by \code{\link{roommate.checkPreferences}}\cr
+#'   \code{topTradingCycle} \tab was replaced by \code{\link{cpp_wrapper_ttc}}\cr
+#'   \code{checkStabilityTopTradingCycle} \tab was replaced by \code{\link{cpp_wrapper_ttc_check_stability}}\cr
+#' }
+#'
+validateInputs = function(...) {
+    .Deprecated(galeShapley.validate)
+    galeShapley.validate(...)
+}
+checkStability = function(...) {
+    .Deprecated(galeShapley.checkStability)
+    galeShapley.checkStability(...)
+}
+checkPreferenceOrder = function(...) {
+    .Deprecated(galeShapley.checkPreferences)
+    galeShapley.checkPreferences(...)
+}
+one2many = function(...) {
+    .Deprecated("galeShapley.collegeAdmissions")
+    galeShapley.collegeAdmissions(..., studentOptimal = TRUE)
+}
+many2one = function(...) {
+    .Deprecated("galeShapley.collegeAdmissions")
+    galeShapley.collegeAdmissions(..., studentOptimal = FALSE)
+}
+one2one = function(...) {
+    .Deprecated("galeShapley.marriageMarket")
+    galeShapley.marriageMarket(...)
+}
+galeShapleyMatching = function(...) {
+    .Deprecated("cpp_wrapper_galeshapley")
+    cpp_wrapper_galeshapley(...)
+}
+stableRoommateMatching = function(...) {
+    .Deprecated("cpp_wrapper_irving")
+    cpp_wrapper_irving(...)
+}
+onesided = function(...) {
+    .Deprecated("roommate.matching")
+    roommate.matching(...)
+}
+checkStabilityRoommate = function(...) {
+    .Deprecated("cpp_wrapper_irving_check_stability")
+    cpp_wrapper_irving_check_stability(...)
+}
+validateInputsOneSided = function(...) {
+    .Deprecated("roommate.validate")
+    roommate.validate(...)
+}
+checkPreferenceOrderOnesided = function(...) {
+    .Deprecated("roommate.checkPreferences")
+    roommate.checkPreferences(...)
+}
+topTradingCycle = function(...) {
+    .Deprecated("cpp_wrapper_ttc")
+    cpp_wrapper_ttc(...)
+}
+checkStabilityTopTradingCycle = function(...) {
+    .Deprecated("cpp_wrapper_ttc_check_stability")
+    toptrading.checkStability(...)
+}
+NULL
--- R/toptradingcycle.R
+++ R/toptradingcycle.R
@@ -26,29 +26,3 @@ toptrading.checkStability = function(pref, matchings) {
     args = galeShapley.validate(proposerPref = pref, reviewerPref = pref, proposerUtils = NULL, reviewerUtils = NULL)
     cpp_wrapper_ttc_check_stability(args$proposerPref, matchings-1)
 }
-
-#' C++ wrapper for top trading cycle algorithm (Deprecated)
-#'
-#' This function uses the top trading cycle algorithm to find a stable trade between agents,
-#' each with some indivisible good, and with preferences over the goods of other agents. Each
-#' agent is matched to one other agent, and matchings are not necessarily two-way. Agents may
-#' be matched with themselves.
-#'
-#' @param pref A matrix with agent's cardinal preferences. Column i is agent i's preferences.
-#' @return A list with the matchings made. The matchings are encoded as follows: The first value
-#' in the list is the individual to whom agent 1 will be giving his good, the second value in the list
-#' is the individual to whom agent 2 will be giving his good, etc.
-topTradingCycle = function(pref) {
-    .Deprecated("cpp_wrapper_ttc")
-    cpp_wrapper_ttc(pref)
-}
-
-#' Check if a one-sided matching for the top trading cycle algorithm is stable (Deprecated)
-#'
-#' @param pref is a matrix with ordinal rankings of the participants
-#' @param matchings is an nx1 matrix encoding who is matched to whom using
-#' R style indexing
-#' @return true if the matching is stable, false otherwise
-checkStabilityTopTradingCycle = function(pref, matchings) {
-    .Deprecated("cpp_wrapper_ttc_check_stability")
-}
