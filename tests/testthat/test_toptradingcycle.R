4a6f46b6ea4e2f9a141b3e053c904b28fc332d10
Jan Tilly <jantilly@gmail.com>
2015-10-02 12:50:55 -0400

    Fixed test and vignette

--- tests/testthat/test_toptradingcycle.R
+++ tests/testthat/test_toptradingcycle.R
@@ -5,8 +5,7 @@ test_that("Stable?", {
     set.seed(1)
     for (i in c(4, 8, 16, 32, 128, 256)) {
         utils = replicate(i, rnorm(i))
-        args = galeShapley.validate(proposerPref = NULL, reviewerPref = NULL, proposerUtils = utils, reviewerUtils = utils)$proposerPref
-        results = toptrading.matching(pref = args)
-        expect_true(toptrading.checkStability(args, results))
+        results = toptrading.matching(utils = utils)
+        expect_true(toptrading.checkStability(utils = utils, matchings = results))
     }
 })
