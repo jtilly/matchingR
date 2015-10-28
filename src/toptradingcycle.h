f6aad7684c64abdf189e1e68b1d070f63c32e1f8
Jan Tilly <jantilly@gmail.com>
2015-10-28 10:45:16 -0400

    updated copyright note

--- src/toptradingcycle.h
+++ src/toptradingcycle.h
@@ -1,19 +1,19 @@
-//'  matchingR -- Efficient Computation of Matching Algorithms in R
-//'
-//'  Copyright (C) 2015  Jan Tilly <jtilly@econ.upenn.edu>
-//'                      Nick Janetos <njanetos@econ.upenn.edu>
-//'
-//'  This file is part of matchingR.
-//'
-//'  matchingR is free software: you can redistribute it and/or modify
-//'  it under the terms of the GNU General Public License as published by
-//'  the Free Software Foundation, either version 2 of the License, or
-//'  (at your option) any later version.
-//'
-//'  matchingR is distributed in the hope that it will be useful,
-//'  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//'  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//'  GNU General Public License for more details.
+//  matchingR -- Matching Algorithms in R and C++
+//
+//  Copyright (C) 2015  Jan Tilly <jtilly@econ.upenn.edu>
+//                      Nick Janetos <njanetos@econ.upenn.edu>
+//
+//  This file is part of matchingR.
+//
+//  matchingR is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  matchingR is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
 
 #ifndef toptradingcycle_h
 #define toptradingcycle_h
